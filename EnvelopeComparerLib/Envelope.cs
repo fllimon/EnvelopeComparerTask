9f1f61101b7ec775b25471c2863062ce0b2b677c
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-04-13 17:42:25 +0300

    Add project files.

---
+++ EnvelopeComparerLib/Envelope.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvelopeComparerLib
+{
+    public class Envelope
+    {
+        #region ======------ PRIVATE DATA ------=======
+
+        private double _a = -1;
+        private double _b = -1;
+
+        #endregion
+
+        #region ======------- CTOR ---------=======
+
+        public Envelope(double a, double b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        #endregion
+
+        #region =====------ PROPERTIES ------======
+
+        public double A
+        {
+            get 
+            {
+                return _a; 
+            }
+        }
+
+        public double B
+        {
+            get
+            {
+                return _b;
+            }
+        }
+
+        #endregion
+    }
+}
